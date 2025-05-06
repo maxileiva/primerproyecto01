41541bcb29c7f0e3427600e761f21a153c7e38a1
Maxi Leiva <max.leiva@duocuc.cl>
2025-05-06 15:15:35 -0400

    01

---
+++ src/main/java/com/example/pp1/Pp1Application.java
@@ -0,0 +1,13 @@
+package com.example.pp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp1Application.class, args);
+	}
+
+}
